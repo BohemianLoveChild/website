d761cb3ab06f848b84ab8f0e868e8215c4da54a7
BohemianLoveChild <rhythmandsol@gmail.com>
2015-01-18 23:30:37 -0500

    embed tweaks

--- .build/.wh/searchjson.js
+++ .build/.wh/searchjson.js
@@ -1,6 +1,11 @@
 var tipuesearch = {"pages": [
 {"title":"Webhook site - test","text":"","tags":"","loc":"/embeds/test/"},
 {"title":"Webhook site - embeds","text":"","tags":"","loc":"/embeds/"},
+{"title":"Webhook site - Aint Nobody","text":"","tags":"","loc":"/musicvideos/aint-nobody/"},
+{"title":"Webhook site - musicvideos","text":"","tags":"","loc":"/musicvideos/"},
+{"title":"Webhook site - Aint Nobody","text":"","tags":"","loc":"/songs/aint-nobody/"},
+{"title":"Webhook site - Get Down","text":"","tags":"","loc":"/songs/get-down/"},
+{"title":"Webhook site - songs","text":"","tags":"","loc":"/songs/"},
 {"title":"Webhook","text":"","tags":"","loc":"/cms/"},
 {"title":"Webhook site - Homepage","text":"Aint Nobody\n      \n\n      \n        \n      \n    \n\n    \n    \n\n    \n      \n        \t\t\n\t\n\t\tFlavors\n        Music Videos\n        Your Turn\n        Remixes\n        Whodat\n        Swag\n      \n    \n\n    \n      \n        \t\t\n\t\n\t\tFlavors\n        Music Videos\n        Your Turn\n        Remixes\n        Whodat\n        Swag\n      \n    \n\n    \n\n\t\t\t\n\n\n\n\n  \n    \n\n    \n-->\n\n    \n  \n\n\n\n\n\t\n\t\tThis is the crawler.","tags":"","loc":"/"},
 {"title":"Webhook site","text":"","tags":"","loc":"/search-results/"},
