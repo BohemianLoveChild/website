c8e7b4ac16a63a3fdbf0ae40e04491377d232b4f
BohemianLoveChild <rhythmandsol@gmail.com>
2015-01-25 16:30:32 -0500

    bouncy play button

--- .build/static/javascript/animation.js
+++ .build/static/javascript/animation.js
@@ -1,3 +1,15 @@
-function init() {}
-TweenMax.to(embed-social-button, 2, {css:{left:xc}, ease:Expo.easeOut});
-}
+$(function() {
+					var buttonRight = $(".embed-social-button-right"),
+						buttonLeft = $(".embed-social-button-left"),
+						buttonMiddle = $(".play-button"),
+						iframe = $(".youtube-iframe"),
+					
+						tlButtonsAppear = new TimelineMax();
+
+						tlButtonsAppear.timeScale();
+					
+						tlButtonsAppear.staggerFrom(buttonLeft, 3, {scale: "0", right: "67%", rotation: "45", ease:Elastic.easeOut, delay:0.75}, 0.25)
+									.staggerFrom(buttonRight, 3, {scale: "0", left: "67%", rotation: "45", ease:Elastic.easeOut}, 0.25, "-=3")
+									.from(buttonMiddle, 3, {scale: "0", bottom: "67%", rotation: "45", ease:Elastic.easeOut}, "-=2.75")
+									.to(buttonMiddle, 0.5, {scale: "0.9", rotation: "5", repeat:-1, yoyo:true, ease:Sine.easeInOut}, "-=1.75") ;
+});	
