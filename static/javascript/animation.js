81fc7ccd254a81ab087467c39adb036902f493da
BohemianLoveChild <rhythmandsol@gmail.com>
2015-01-25 20:28:36 -0500

    tweak animation timing

--- static/javascript/animation.js
+++ static/javascript/animation.js
@@ -5,11 +5,17 @@ $(function() {
 						iframe = $(".youtube-iframe"),
 					
 						tlButtonsAppear = new TimelineMax();
+						tlBounceLoop = new TimelineMax({repeat:-1,repeatDelay:2, ease:Sine.easeInOut});
 
 						tlButtonsAppear.timeScale();
 					
 						tlButtonsAppear.staggerFrom(buttonLeft, 3, {scale: "0", right: "67%", rotation: "45", ease:Elastic.easeOut, delay:0.75}, 0.25)
 									.staggerFrom(buttonRight, 3, {scale: "0", left: "67%", rotation: "45", ease:Elastic.easeOut}, 0.25, "-=3")
 									.from(buttonMiddle, 3, {scale: "0", bottom: "67%", rotation: "45", ease:Elastic.easeOut}, "-=2.75")
-									.to(buttonMiddle, 0.5, {scale: "0.9", rotation: "5", repeat:-1, yoyo:true, ease:Sine.easeInOut}, "-=1.75") ;
+									.to(buttonMiddle, 0.5, {scale: "0.9", rotation: "5", repeat:-1, yoyo:true, ease:Sine.easeInOut}, "loop-=1.7")
+									.staggerTo(buttonLeft, 3, {rotation: "360", repeat:-1, yoyo:true, repeatDelay:15, ease:Sine.easeInOut}, 1.15, "loop")
+									.staggerTo(buttonRight, 3, {rotation: "360", repeat:-1, yoyo:true, repeatDelay:15, ease:Sine.easeInOut}, 1.15, "loop+=3");
+
+
+
 });	
